317c856206b11f741eecd1565a88346b2a8f528d
maxalexandrov26 <max.aleks@icloud.com>
2022-10-11 14:27:33 +0100

    approximating pi

--- lecture2/practice_file.py
+++ lecture2/practice_file.py
@@ -1,2 +1,18 @@
+import numpy.linalg as la
 import numpy as np
+from numpy.polynomial import Polynomial
+denominators = np.arange(1,10000)
+signs = np.ones((denominators.shape[0],))
+signs[0:-1:4] = -1
+signs = -signs
+coeff = signs/denominators
+coeff[1:-1:2] = 0
+arctan_series = Polynomial(coeff)
+print(4*arctan_series(1))
+
+
+
+
+
+
 
